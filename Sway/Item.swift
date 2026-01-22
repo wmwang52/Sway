00ccab76d0c16566e9fed6b8db91273b811be93b
William Wang <wmwang52@gmail.com>
2026-01-22 16:33:51 -0500

    Added Initial Files

---
+++ Sway/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sway
+//
+//  Created by William Wang on 1/22/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwayTests/SwayTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwayTests.swift
+//  SwayTests
+//
+//  Created by William Wang on 1/22/26.
+//
+
+import Testing
+@testable import Sway
+
+struct SwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
